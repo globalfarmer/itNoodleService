63fe01f78dc9d2f8adcf8226c179d332d84a28b9
globalfarmer <dovietanh.95@gmail.com>
2018-05-24 17:05:22 +0700

    issue#15 change student api

--- routes/student.js
+++ routes/student.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var itnoodle = require('../project_modules/itnoodle.js');
 var copy_value_by_fields = function(fields, src, dst) {
 	fields.forEach((f) => {
-		dst[f] = src[f];
+		dst[f[1]] = src[f[0]];
 	});
 }
 router.get('/', (req, res) => {
@@ -21,6 +21,31 @@ router.get('/', (req, res) => {
 			let course_codes = Object.keys(std.slots);
 			// console.log(course_codes);
 			// TODO validate term, year
+			if(!std.hasOwnProperty("finaltests"))
+				std.finaltests = {};
+			let _day, _room, _milliseconds, _tmp;
+			course_codes.forEach((cc) => {
+				_tmp = std.slots[cc].code.toUpperCase();
+				if(_tmp.length>7)
+					std.slots[cc].code = [_tmp.slice(0, 7), _tmp.slice(7)].join("-");
+				else
+					std.slots[cc].code = _tmp.slice(0, 7);
+				if(std.finaltests.hasOwnProperty(cc)) {
+					_day = std.finaltests[cc].time.split(":");
+					_tmp = std.finaltests[cc].day;
+					_milliseconds = _tmp.getTime() + ((parseInt(_day[0])*60)+parseInt(_day[1]))*60000 + 7*60*60000;
+					_day = [std.finaltests[cc].time, [("0"+(_tmp.getDate()+1)).slice(-2), ("0"+(_tmp.getMonth()+1)).slice(-2), _tmp.getFullYear()].join("-")].join(" ");
+					_room = [std.finaltests[cc].room, std.finaltests[cc].building].join(" - ");
+					std.slots[cc].day = _day;
+					std.slots[cc].time = _milliseconds;
+					std.slots[cc].room = _room;
+					std.slots[cc].type = std.finaltests[cc].type;
+				}
+				else
+					std.slots[cc].time = 0;
+			})
+			course_codes.sort((a, b) => std.slots[a].time > std.slots[b].time);
+			std.course_cods = course_codes;
 			std.sbs = {};
 			itnoodle
 			.scoreboardCol
@@ -31,9 +56,9 @@ router.get('/', (req, res) => {
 						// console.log(sb.course);
 						std.sbs[sb.course.code] = {};
 						copy_value_by_fields(
-							["public_link", "uploadtime"],
+							[["src", "public_link"], ["uploadtime", "uploadtime"]],
 							sb.course,
-							std.sbs[sb.course.code]
+							std.slots[sb.course.code]
 						)
 					})
 				else
